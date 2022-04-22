9be51d4f781989fb0b564143d54007cb06253ec1
Pranavchiku <goswami.4@iitj.ac.in>
2022-04-22 19:40:26 +0530

    commit

--- script.js
+++ script.js
@@ -59,28 +59,28 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     }
   
     // First time - there is no thumbnail element, so lets create it
-    if (!thumbnailElement) {
-      thumbnailElement = document.createElement("div");
-      thumbnailElement.classList.add("drop-zone__thumb");
-      dropZoneElement.appendChild(thumbnailElement);
-    }
+    // if (!thumbnailElement) {
+    //   thumbnailElement = document.createElement("div");
+    //   thumbnailElement.classList.add("drop-zone__thumb");
+    //   dropZoneElement.appendChild(thumbnailElement);
+    // }
   
-    thumbnailElement.dataset.label = file.name;
+    // thumbnailElement.dataset.label = file.name;
   
     // Show thumbnail for image files
-    if (file.type.startsWith("image/")) {
-      const reader = new FileReader();
+    // if (file.type.startsWith("image/")) {
+    //   const reader = new FileReader();
   
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-        var image=document.getElementsByName("myFile");
-        console.log(image);
-        image.src=reader.result;
-      };
-    } else {
-      thumbnailElement.style.backgroundImage = null;
-    }
+    //   reader.readAsDataURL(file);
+    //   reader.onload = () => {
+    //     thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    //     var image=document.getElementsByName("myFile");
+    //     console.log(image);
+    //     image.src=reader.result;
+    //   };
+    // } else {
+    //   thumbnailElement.style.backgroundImage = null;
+    // }
   }
   
 
